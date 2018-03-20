193b0146c195a7fea926d68336a3a10a8010a7de
dyyao <646617513@qq.com>
2018-03-20 10:24:36 +0800

    first commit

---
+++ database/config.js
@@ -0,0 +1,1 @@
+var mongoose = require('mongoose');mongoose.connect('mongodb://localhost/myblog');db = mongoose.connection;db.on('error', console.error.bind(console, 'connection error:'));db.once('open', function() {    // we're connected!    console.log('数据库连接成功')});
